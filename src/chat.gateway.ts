56e51e8ac61385b8a038384df5245fac408cd5ce
Adeleke Oluwatobi <tobiadiks@gmail.com>
2024-07-11 16:33:37 +0100

    update

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,15 +1,41 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 // can specify port and namespace
 // @WebSocketGateway(80,{namespace:'chat'})
-@WebSocketGateway(3002,{cors:{origin:'http://localhost:3001',credentials:true}})
+@WebSocketGateway(3002, {
+  cors: { origin: 'http://localhost:3001', credentials: true },
+})
 export class ChatGateway {
-    // accessing server clients
-    @WebSocketServer()
-    server;
-    // add all event handlers and broadcast to all listeners
-    @SubscribeMessage('message')
-    handleMessage(@MessageBody() message: string): void {
-        // sending message to everyone on the server
-        this.server.emit('message',message)
+  // accessing server clients
+  @WebSocketServer()
+  server: Server;
+  // add all event handlers and broadcast to all listeners
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    // sending message to everyone on the server
+    this.server.emit('message', message);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Client connected: ${client.id}`);
+    // Handle authentication here
+    const token = client.handshake.headers.authorization.split(' ')[1];
+    if (!token) {
+      client.disconnect(true);
+      return;
     }
+    try {
+      const decoded = this.jwtService.verify(token);
+      // Optionally, you can attach the user object to the client object for use in subsequent events
+      client['user'] = decoded;
+    } catch (err) {
+      console.error('Invalid token');
+      client.disconnect(true);
+    }
+  }
 }
